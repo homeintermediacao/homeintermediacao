3efe777a5f15c8168e8e4c796d41ef1f437f4e4a
bruno-desk\teste <bmgmadruga@hotmail.com>
2019-03-31 07:31:42 -0300

    Versao inicial
    Versão inicial para teste de publicação

---
+++ post.php
@@ -0,0 +1,19 @@
+<?php
+session_start();	
+$name = $_GET['name'];
+$email = $_GET['email'];
+$message = $_GET['message'];
+$subject = '[Mensagem do site], de ' . $name;
+if(strtolower($_REQUEST['code']) == strtolower($_SESSION['random_number']))
+{
+$TO = "bmgmadruga@hotmail.com";
+$h = "From: " . $email;
+$content = "$name ($email) enviou uma nova mensagem :\n\n$message";
+mail($TO, $subject, $content, $h);		
+	echo 1;		
+}	
+else
+{
+	echo 0; // invalid code
+}
+?>
