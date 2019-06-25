b3b1187f56fa50f1307d1d726396659c83f25908
homeintermediacao <bmgmadruga@hotmail.com>
2019-06-24 21:50:09 -0300

    teste de email
    Alterando email para teste

--- post.php
+++ post.php
@@ -6,10 +6,11 @@ $message = $_GET['message'];
 $subject = '[Mensagem do site], de ' . $name;
 if(strtolower($_REQUEST['code']) == strtolower($_SESSION['random_number']))
 {
-$TO = "contato@homeintermediacao.com.br";
-$h = "From: " . $email;
-$content = "$name ($email) enviou uma nova mensagem :\n\n$message";
-mail($TO, $subject, $content, $h);		
+	//$TO = "contato@homeintermediacao.com.br";
+	$TO = "bmgmadruga@hotmail.com";
+	$h = "From: " . $email;
+	$content = "$name ($email) enviou uma nova mensagem :\n\n$message";
+	mail($TO, $subject, $content, $h);		
 	echo 1;		
 }	
 else
