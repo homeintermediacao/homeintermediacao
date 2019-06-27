03ac77ab298b6f15e48a8895bfb238c50e176d05
homeintermediacao <bmgmadruga@hotmail.com>
2019-06-26 22:03:46 -0300

    teste email TO
    teste email TO

--- post.php
+++ post.php
@@ -9,8 +9,7 @@ if(strtolower($_REQUEST['code']) == strtolower($_SESSION['random_number']))
 	$TO = "contato@homeintermediacao.com.br";
 	$h = "From: " . $email;
 	$content = "$name ($email) enviou uma nova mensagem :\n\n$message";
-	mail("contato@homeintermediacao.com.br", "Assunto", "Menssagem", "From: email@email.com");
-	mail("bmgmadruga@hotmail.com", "Assunto", "Menssagem", "From: email@email.com");
+	mail($TO, "Assunto", "Menssagem", "From: email@email.com");
 	//mail($TO, $subject, $content, $h);
 	echo 1;		
 }	
